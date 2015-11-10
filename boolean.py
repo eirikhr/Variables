099b0b62c5e39165deda3e0b040db051bae29db3
eirikhr <erynning@gmail.com>
2015-11-10 11:52:32 +0100

    Boolean

--- boolean.py
+++ boolean.py
@@ -1,3 +1,4 @@
+"""
 #Simple number printer
 print("This section prints the numbers from 1 to 10.")
 number = 0
@@ -15,3 +16,15 @@ while True:
     else:
        print(number)
        break
+"""
+# Productivity Alarm
+import time
+import webbrowser
+music_video = str(input("Paste link to media that will autoplay in your webbrowser upon activation"))
+total_breaks = int(input("How many times will the media run?"))
+s = int(input("How often in seconds?"))
+break_count = 0
+while int(break_count) < total_breaks:
+    time.sleep(s)
+    webbrowser.open(music_video)
+    break_count += 1                            #Instead of break_count = break_count + 1
